847cd6de372e5b38d7ce300cd1a1958a8a43ada0
toxicologist <toxiccologist@gmail.com>
2024-07-03 21:19:39 -0300

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="vipassana-timer",
-    version="1.0.3.1",
+    version="1.1",
     author="toxicologist",
     license='MIT',
     author_email="toxiccologist@gmail.com",
